695095bc49a709f4842416884cb8c6ae04106d76
Jeremy Deleon <jeremydeleon455@gmail.com>
2026-06-01 12:44:14 -0500

    Actualizando: Estudiante 2 agrega funcion de valor minimo

--- FunsionG[4].py
+++ FunsionG[4].py
@@ -2,11 +2,24 @@
 # *******************************************************
 # ** Desarrollado por: [Alexis Rangel 8-954-1856] **
 # *******************************************************
-
+# *******************************************************
+# ** Desarrollado por: [Jeremy De León 8-1028-1151] ** 
+# *******************************************************
 # Inicia desarrollo de la función dada
 def tu_funcion_aqui(parametro):
     # Lógica de la función que retorne el resultado
-    resultado = "procesamiento"
+    resultado = "procesamiento"# =========================================================
+# Estudiante 2: [Aquí el Nombre de tu Compañero y su Cédula]
+# Desarrollar una función que reciba una lista y retorne el valor mínimo.
+
+def obtener_valor_minimo(lista):
+    # Lógica para buscar el menor número
+    minimo = min(lista)
+    return minimo
+
+# Ejemplo de prueba (Obligatorio)
+# Regla del print: Su nombre y la salida del resultado
+print(f"[Jeremy De Leon] - Resultado: {obtener_valor_minimo([15, 4, 23, 8, 42])}")
     return resultado
 
 # Compruebe buen funcionamiento (Ejemplo obligatorio)
