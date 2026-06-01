9d8bb9a559e05b4d8d82b0818ec7d1499eb63ac7
Alexis Rangel <alexisran2000@gmail.com>
2026-06-01 11:18:11 -0500

    E1: Agregada funcion inicial

---
+++ FunsionG[4].py
@@ -0,0 +1,14 @@
+# Enunciado: [Grupo 4]
+# *******************************************************
+# ** Desarrollado por: [Alexis Rangel 8-954-1856] **
+# *******************************************************
+
+# Inicia desarrollo de la función dada
+def tu_funcion_aqui(parametro):
+    # Lógica de la función que retorne el resultado
+    resultado = "procesamiento"
+    return resultado
+
+# Compruebe buen funcionamiento (Ejemplo obligatorio)
+# Regla del print: Su nombre y la salida del resultado
+print(f"Alexis Rangel - Resultado: {tu_funcion_aqui('ejemplo')}")
